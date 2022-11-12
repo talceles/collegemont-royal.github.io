e2eea31375b21a0751a97fe380e2832c26cb1f61
Clément Gauthier <clement.gauthier@hotmail.com>
2022-11-12 17:01:08 -0500

    bug fix

---
+++ 404.js
@@ -0,0 +1,30 @@
+import { initialUISetup } from "/site/js/UIHandler.js";
+initialUISetup();
+
+const urlParams = new URLSearchParams(window.location);
+const src = urlParams.get('src');
+if (src) {
+    sendErrorMessage("Source non trouvée au " + src);
+} else {
+    send404Message();
+}
+
+function send404Message() {
+    fetch(
+        "https://cmr-webhooks.herokuapp.com/404" +
+        "?pathname=" + encodeURIComponent(window.location.pathname) +
+        "&useragent=" + encodeURIComponent(window.navigator.userAgent),
+        { method: "POST" }
+    );
+}
+
+function sendErrorMessage(errorDescription) {
+    fetch(
+        "https://cmr-webhooks.herokuapp.com/err?pathname=" + encodeURIComponent(window.location.pathname) + "&src=" + encodeURIComponent(URLUtilities.getSrc()),
+        {
+            method: "POST",
+            headers: { "Content-Type": "text/plain; charset=utf-8" },
+            body: errorDescription,
+        }
+    );
+}
